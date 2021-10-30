29e99ac7c7d077594f2491ed26fdc047f1af0236
athenas2021 <lucas.kano@outlook.com>
2021-10-29 23:42:44 -0300

    Heroku

--- django1/settings.py
+++ django1/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # rodar arquivos estaticos em produção
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
